17da3c26b2e109dc94bfb4adff1c008c44ea1668
Tim Denning <36028402+denningta@users.noreply.github.com>
2022-07-16 12:27:33 +0200

    setup package

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+const config = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+};
+
+export default config;
